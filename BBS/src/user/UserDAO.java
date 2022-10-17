7362db9e5a15a76496c19ad4774112512d84014f
wonsungmoon <wonsungmoon6@gmail.com>
2022-10-17 11:55:30 +0900

    login method 2022/10/17

--- BBS/src/user/UserDAO.java
+++ BBS/src/user/UserDAO.java
@@ -22,5 +22,25 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE user = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword)) {
+					return 1; //로그인 성공
+				}
+				else 
+					return 0; //비밀번호 불일치
+			}
+			return -1; //아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
 
 }
