164d3bfc74e9d3df25b96ca83a1cd150ebc8fba7
wonsungmoon <wonsungmoon6@gmail.com>
2022-10-18 11:25:21 +0900

    bbs Title error solve 2022/10/18

--- BBS/src/bbs/BbsDAO.java
+++ BBS/src/bbs/BbsDAO.java
@@ -78,7 +78,7 @@ public class BbsDAO {
 			while (rs.next()) {
 				Bbs bbs = new Bbs();
 				bbs.setBbsID(rs.getInt(1));
-				bbs.setBbsTitle(rs.getString(1));
+				bbs.setBbsTitle(rs.getString(2));
 				bbs.setUserID(rs.getString(3));
 				bbs.setBbsDate(rs.getString(4));
 				bbs.setBbsContent(rs.getString(5));
@@ -104,5 +104,27 @@ public class BbsDAO {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	public Bbs getBbs(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(1));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				return bbs;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
